726bd688fc9d77dd11a4d88b5630f8072dba2c90
JT <coreprogramm@gmail.com>
2019-12-21 01:22:50 +0530

    Securely-Open-PDF-in-Browser-using-.NET-Core
    Securely-Open-PDF-in-Browser-using-.NET-Core

---
+++ PDFOnBrowser/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PDFOnBrowser.Models;
+
+namespace PDFOnBrowser.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        public JsonResult OpenPDFPath()
+        {
+            string PDFpath = "PDFs/Samplefile.PDF";
+            return Json(PDFpath);
+        }
+        [HttpGet]
+        public FileResult OpenPDF()
+        {
+            string PDFpath = "wwwroot/PDFs/Samplefile.PDF";
+            byte[] abc = System.IO.File.ReadAllBytes(PDFpath);
+            System.IO.File.WriteAllBytes(PDFpath, abc);
+            MemoryStream ms = new MemoryStream(abc);
+            return new FileStreamResult(ms, "application/pdf");
+        }
+    }
+}
